65f956a7d0a5ce9d83e011a9ec7a6221f6e743b4
Joe Hernandez <j.t.hernandez999@gmail.com>
2021-11-20 09:56:21 -0800

    wip

--- api/server.js
+++ api/server.js
@@ -1,5 +1,8 @@
 // build your server here and require it from index.js
 const express = require('express')
+const helmet = require('helmet')
+const cors = require('cors')
+const morgan = require('morgan')
 
 const projectRouter = require('./project/router')
 const resourceRouter = require('./resource/router')
@@ -8,8 +11,11 @@ const taskRouter = require('./task/router')
 const server = express()
 
 require('colors')
-
+server.use(helmet())
+server.use(cors())
+server.use(morgan('dev'))
 server.use(express.json())
+
 server.use('/api/projects', projectRouter)
 server.use('/api/resources', resourceRouter)
 server.use('/api/tasks', taskRouter)
