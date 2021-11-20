4b9194f3ed1be21270babe22862009f176e4fd40
Joe Hernandez <j.t.hernandez999@gmail.com>
2021-11-20 09:51:29 -0800

    set up server and basic router scaffolding

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // start your server here
+require('dotenv').config()
+const server = require('./api/server')
+
+const port = process.env.port || 9000
+
+server.listen(port, () => console.log(`\nAPI running on port ${port}!!`.bgBlue))
