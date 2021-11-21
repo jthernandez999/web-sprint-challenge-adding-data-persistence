ca94c51740d2710f87f9add9642c4c317776ad19
Joe Hernandez <j.t.hernandez999@gmail.com>
2021-11-21 09:39:17 -0800

    wip

--- api/project/router.js
+++ api/project/router.js
@@ -2,5 +2,36 @@
 const router = require('express').Router()
 const Project = require('./model')
 
+router.get('/', (req, res, next) => {
+    Project.getAll()
+        .then(projects => {
+            res.json(projects)
+        })
+        .catch(next)
+})
+
+router.post('/', (req, res, next) => {
+    const newProject = req.body
+    Project.insert(newProject)
+        .then(project => {
+            if(!project.project_name){
+                res.status(400).json({
+                    message: 'project_name required'
+                })
+            } else {
+                res.status(201).json(project)
+            }
+        })
+        .catch(next)
+})
+
+
+router.use((err, req, res, next) => {
+    res.status(err.status || 500).json({
+        customMessage: 'something went terribly wrong',
+        message: err.message, 
+        stack: err.stack,
+    })
+})
 
 module.exports = router 
